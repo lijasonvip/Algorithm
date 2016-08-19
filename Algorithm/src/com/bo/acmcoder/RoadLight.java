d0715949d137699df8a1b5d0d2612bf9935a1bf9
lijasonvip <einstatham@163.com>
2016-08-20 00:58:39 +0800

    update

--- Algorithm/src/com/bo/acmcoder/RoadLight.java
+++ Algorithm/src/com/bo/acmcoder/RoadLight.java
@@ -11,6 +11,35 @@ public class RoadLight {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int len = sc.nextInt();
+		int[] a = new int[n];
+		for(int i=0;i<n;i++){
+			a[i] = sc.nextInt();
+		}
+		Arrays.sort(a);
+		int max = Integer.MIN_VALUE;
+		for (int i = 1; i < a.length; i++) {
+			if (a[i] - a[i-1] > max) {
+				max = a[i] - a[i-1];
+			}
+		}
+		
+		if (a[0] != 0) {
+			max = Math.max(max, a[0]);
+		}
+		
+		if (a[n-1] != len) {
+			max = Math.max(max, a[n-1]);
+		}
+		
+		//注意输出格式
+		DecimalFormat df = new DecimalFormat("0.00");
+		System.out.println(df.format(max));
+	}
+
+	public static void light2(){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int len = sc.nextInt();
 		int[] spot = new int[n];
 		for (int i = 0; i < n; i++)
 			spot[i] = sc.nextInt();
@@ -30,7 +59,6 @@ public class RoadLight {
 			System.out.printf("%.2f", maxdistance / 2);
 		}
 	}
-
 	public static void light() {
 		Scanner in = new Scanner(System.in);
 		DecimalFormat df = new DecimalFormat("0.00");
