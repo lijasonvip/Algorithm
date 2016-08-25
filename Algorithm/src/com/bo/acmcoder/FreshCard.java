690eba7d77a9f913234350b081e83d11463d3ecd
lijasonvip <einstatham@163.com>
2016-08-25 22:19:23 +0800

    update

---
+++ Algorithm/src/com/bo/acmcoder/FreshCard.java
@@ -0,0 +1,14 @@
+package com.bo.acmcoder;
+
+import java.util.Scanner;
+
+public class FreshCard {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+	}
+	public static int fresh(){
+		return 0;
+	}
+}
