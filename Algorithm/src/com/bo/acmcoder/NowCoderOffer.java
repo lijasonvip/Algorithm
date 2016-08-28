948e7e88fe89e0a5ebaf8414a4069afc2ee9794f
lijasonvip <einstatham@163.com>
2016-08-28 16:22:45 +0800

    update

--- Algorithm/src/com/bo/acmcoder/NowCoderOffer.java
+++ Algorithm/src/com/bo/acmcoder/NowCoderOffer.java
@@ -1,36 +1,159 @@
 package com.bo.acmcoder;
 
+import java.util.ArrayList;
+import java.util.Currency;
+import java.util.List;
 import java.util.Stack;
 
 public class NowCoderOffer {
 
 	// 牛客网的剑指offer
- 
+
+	public static void main(String[] args) {
+		int[] data = { 1, 2, 3, 4, 5};
+		ListNode head = construct(data);
+		System.out.println(FindKthToTail(head, 6).val);
+	}
 	
+	//反转链表
+	public static ListNode ReverseList(ListNode head){
+		ListNode dummy = new ListNode(-1);
+		dummy.next = head;
+	}
 	
-	//统计二进制数中1的个数
-	//http://www.360doc.com/content/15/1108/16/1317564_511672961.shtml
-	int CountOne(int n){
+	//链表倒数第k个节点
+	//考虑k大于链表长度 输入为空的情况
+	public static ListNode FindKthToTail(ListNode head, int k){
+		if (head == null) {
+			return null;
+		}
+		ListNode temp = head;
+		while(temp.next != null && k > 1) {
+			temp = temp.next;
+			k--;
+		}
+		if (k != 1) {
+			return null;
+		}
+		while(temp.next != null){
+			temp = temp.next;
+			head = head.next;
+		}
+		return head;
+	}
+	
+	public static ListNode construct(int[] data){
+		ListNode head = new ListNode(data[0]);
+		ListNode cur = head;
+		for (int i = 1; i < data.length; i++) {
+			cur.next = new ListNode(data[i]);
+			cur = cur.next;
+		}
+		return head;
+	}
+
+	// 调整数组 使得奇数在偶数的前面
+	public static void reOrderArray_NotStable(int[] array) {
+		// 快排划分的方法不稳定 不能保证原来奇偶数的顺序
+		int i = 0, j = array.length - 1;
+		while (i < j) {
+			while (i < j && array[i] % 2 == 1)
+				i++;
+			while (i < j && array[j] % 2 == 0)
+				j--;
+			if (i < j) {
+				int temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+			}
+		}
+	}
+
+	/**
+	 * 1.要想保证原有次序，则只能顺次移动或相邻交换。 2.i从左向右遍历，找到第一个偶数。 3.j从i+1开始向后找，直到找到第一个奇数。
+	 * 4.将[i,...,j-1]的元素整体后移一位，最后将找到的奇数放入i位置，然后i++。 5.終止條件：j向後遍歷查找失敗。
+	 */
+	public static void reOrderArray_Stable(int[] array) {
+		for (int i = 0; i < array.length - 1; i++) {
+			for (int j = 0; j < array.length - 1 - i; j++) {
+				if (array[j] % 2 == 0 && array[j + 1] % 2 == 1) {
+					int t = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = t;
+				}
+			}
+		}
+	}
+	
+	public static void reOrderArray2(int[] array){
+		List<Integer> odd = new ArrayList<>();
+		List<Integer> even = new ArrayList<>();
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] % 2 == 0) {
+				even.add(array[i]);
+			}
+			else{
+				odd.add(array[i]);
+			}
+		}
+		int i=0;
+		for(int e : odd){
+			array[i++] = e;
+		}
+		for(int e:even){
+			array[i++] = e;
+		}
+	}
+
+	// 数值的整数次方 代码的完整性 考虑周全 整数负数0等
+	public static double exposional(double base, int exponent) {
+		double temp = 1;
+		if (exponent > 0) {
+			for (int i = 1; i <= exponent; i++) {
+				temp = temp * base;
+				if (temp > Double.MAX_VALUE) {
+					return -1;
+				}
+			}
+			return temp;
+		} else if (exponent < 0) {
+			exponent = -exponent;
+			for (int i = 1; i <= exponent; i++) {
+				temp = temp * base;
+				if (temp > Double.MAX_VALUE) {
+					return -1;
+				}
+			}
+			temp = 1.0 / temp;
+			return temp;
+		} else {
+			return 1;
+		}
+	}
+
+	// 统计二进制数中1的个数
+	// http://www.360doc.com/content/15/1108/16/1317564_511672961.shtml
+	int CountOne(int n) {
 		int count = 0;
-		while(n != 0){
-			n = n & (n-1);
-			count ++;
+		while (n != 0) {
+			n = n & (n - 1);
+			count++;
 		}
 		return count;
 	}
-	
-	int CountOne2(int n){
+
+	int CountOne2(int n) {
 		int count = 0;
 		int flag = 1;
-		while(flag > 0){
+		while (flag > 0) {
 			count += (n & flag) != 0 ? 1 : 0;
 			flag = flag << 1;
 		}
 		return count;
 	}
-	
-	// 变态青蛙跳  可以跳 1 2 ... n 个  动态规划
-	//http://www.nowcoder.com/questionTerminal/22243d016f6b47f2a6928b4313c85387
+
+	// 变态青蛙跳 可以跳 1 2 ... n 个 动态规划
+	// http://www.nowcoder.com/questionTerminal/22243d016f6b47f2a6928b4313c85387
 	public int JumpFloorII(int target) {
 		if (target == 0) {
 			return 0;
@@ -136,4 +259,17 @@ class NowCodeStack {
 			return stack2.pop();
 		}
 	}
+}
+
+class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+    
+    public String toString(){
+    	return String.valueOf(this.val);
+    }
 }
