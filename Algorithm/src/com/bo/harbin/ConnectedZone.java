bfd1290a3486855794d51baaee457d56b2bec4a8
lijasonvip <einstatham@163.com>
2016-09-13 23:21:41 +0800

    update

---
+++ Algorithm/src/com/bo/harbin/ConnectedZone.java
@@ -0,0 +1,14 @@
+package com.bo.harbin;
+
+import java.util.ArrayList;
+
+public class ConnectedZone {
+
+	public static int countZones(int[][] matrix){
+		int row = matrix.length;
+		int col = matrix[0].length;
+		int[][] flag = new int[row][col];
+		return 0;
+	}
+	
+}
