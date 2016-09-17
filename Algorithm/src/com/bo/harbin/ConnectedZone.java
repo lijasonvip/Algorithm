dc29f7d1cd6d0a596633ec2723c03393ce44998d
lijasonvip <einstatham@163.com>
2016-09-17 10:43:39 +0800

    *

--- Algorithm/src/com/bo/harbin/ConnectedZone.java
+++ Algorithm/src/com/bo/harbin/ConnectedZone.java
@@ -1,6 +1,8 @@
 package com.bo.harbin;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class ConnectedZone {
 
@@ -8,7 +10,21 @@ public class ConnectedZone {
 		int row = matrix.length;
 		int col = matrix[0].length;
 		int[][] flag = new int[row][col];
+		
 		return 0;
 	}
 	
+	public static void bfs(int[][] matrix, int start){
+		ArrayList<Integer> res = new ArrayList<>();
+		int[] visited = new int[matrix.length];
+		Queue<Integer> queue = new LinkedList<>();
+		queue.offer(start);
+		visited[start] = 1;
+		while(!queue.isEmpty()){
+			
+		}
+	}
+	
 }
+
+
